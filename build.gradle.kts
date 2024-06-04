7eb63f264693a798955a6cd04993ba2395530518
Lucas Diniz Frota <lucasdinizfrota@gmail.com>
2024-06-04 18:16:11 -0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,73 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.espacocultural"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.espacocultural"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    implementation ("androidx.core:core:1.13.1")
+//    implementation("androidx.core:core-ktx:1.13.1")
+//    implementation("androidx.core:core-testing:1.13.1")
+
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
+
+    implementation("com.google.android.material:material:1.9.0")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+    implementation("com.google.firebase:firebase-firestore-ktx:24.11.1")
+//    implementation("com.google.cloud:google-cloud-translate:1.94.3")
+//    implementation("com.google.api-client:google-api-client-android:1.33.0")
+//    implementation("com.google.auth:google-auth-library-oauth2-http:1.7.0")
+//    implementation("com.google.http-client:google-http-client-gson:1.39.2")
+
+    implementation("com.google.zxing:core:3.4.0")
+    implementation("com.journeyapps:zxing-android-embedded:4.2.0")
+
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
